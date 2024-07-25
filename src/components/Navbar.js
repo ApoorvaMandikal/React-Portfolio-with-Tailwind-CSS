25b6a11accb87d6e6ae3e1cbd7b04c7bb9ec66cb
Apoorva Mandikal <apoorvagirish76@gmail.com>
2024-07-25 13:14:17 +0000

    feat: added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,23 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
-export default function Navbar() {
-
+export default function Navbar({ scrollToSection, aboutRef, skillsRef, projectsRef, contactRef }) {
+    return (
+        <nav className="bg-gray-800 p-4 fixed w-full z-10">
+          <ul className="flex justify-end space-x-4">
+            <li>
+              <button onClick={() => scrollToSection(aboutRef)} className="text-white">About</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection(skillsRef)} className="text-white">Skills</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection(projectsRef)} className="text-white">Projects</button>
+            </li>
+            <li>
+              <button onClick={() => scrollToSection(contactRef)} className="text-white">Contact</button>
+            </li>
+          </ul>
+        </nav>
+      );
 }
